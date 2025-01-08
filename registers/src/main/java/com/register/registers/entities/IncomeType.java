ccf6a3c5290dcb9c2127fb58d2bd2fb0009a2360
JulioErives <julioerives345@gmail.com>
2025-01-07 17:26:43 -0800

    fix: add user ID field to IncomeType entity

--- registers/src/main/java/com/register/registers/entities/IncomeType.java
+++ registers/src/main/java/com/register/registers/entities/IncomeType.java
@@ -1,15 +1,23 @@
 package com.register.registers.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class IncomeType {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long incomeTypeId;
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    @JsonIgnore
+    private Users users;
     private String name;
     private String description;
     private String colorRgba;
