a2095847a466c5dfec5076d9b379c40d77e410fc
JulioErives <julioerives345@gmail.com>
2025-04-18 16:09:48 -0700

    feat: add validators to eventsDTO

--- registers/src/main/java/com/register/registers/dto/EventsDTO.java
+++ registers/src/main/java/com/register/registers/dto/EventsDTO.java
@@ -2,6 +2,11 @@ package com.register.registers.dto;
 
 import java.time.LocalDate;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,10 +16,17 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class EventsDTO {
     private Long eventId; 
+    @NotNull(message = "La fecha inicio es obligatoria")
+    @FutureOrPresent( message = "La fecha inicio no puede estar en el pasado")
     private LocalDate startDate;
+    @NotNull(message = "La fecha fin es obligatoria")
+    @Future( message = "La fecha no puede estar en el pasado")
     private LocalDate endDate;
+    @NotNull(message = "Tienes que escoger el campo de notificaciones del telefono")
     private Boolean phoneNotifications;
+    @NotNull(message = "Tienes que escoger el campo de notificaciones de el sitio web")
     private Boolean webNotificacions;
+    @NotNull(message = "El campo de minutos del aviso es obligatorio")
     private int minutesAdvice;
 
 }
