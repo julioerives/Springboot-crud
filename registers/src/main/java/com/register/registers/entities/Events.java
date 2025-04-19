035402e32629727eb2e96451c560991e69e15296
JulioErives <julioerives345@gmail.com>
2025-04-18 21:14:04 -0700

    feat: add missing @Entity annotation to Events class

--- registers/src/main/java/com/register/registers/entities/Events.java
+++ registers/src/main/java/com/register/registers/entities/Events.java
@@ -8,10 +8,12 @@ import org.hibernate.annotations.UpdateTimestamp;
 
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
+@Entity
 @Data
 public class Events {
     @Id
