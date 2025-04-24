8a713ddfbd0c9b9688869a29dff7a6374a225c0b
Julio Erives <julioerives345@gmail.com>
2025-04-24 08:22:25 -0600

    refactor: remove unused imports in GlobalExceptionHandler

--- registers/src/main/java/com/register/registers/exceptions/GlobalExceptionHandler.java
+++ registers/src/main/java/com/register/registers/exceptions/GlobalExceptionHandler.java
@@ -1,13 +1,10 @@
 package com.register.registers.exceptions;
 
 
-import java.util.HashMap;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
