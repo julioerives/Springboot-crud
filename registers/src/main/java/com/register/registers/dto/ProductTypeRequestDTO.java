c5ddb468654cff8ed1ecc44e70a7725833be5575
JulioErives <julioerives345@gmail.com>
2025-05-01 10:53:33 -0700

    feat: add validation annotations to ProductTypeRequestDTO

--- registers/src/main/java/com/register/registers/dto/ProductTypeRequestDTO.java
+++ registers/src/main/java/com/register/registers/dto/ProductTypeRequestDTO.java
@@ -1,5 +1,6 @@
 package com.register.registers.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,8 +9,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ProductTypeRequestDTO {
-    private Long userId;
+    @NotNull(message = "EL nombre del tipo de producto es requerido")
     private String name;
+    @NotNull(message = "La descripcion del tipo de producto es requerido")
     private String description;
+    @NotNull(message = "La descripcion del tipo de producto es requerido")
     private String colorRgba;
 }
