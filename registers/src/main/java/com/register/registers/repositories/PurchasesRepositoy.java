310d30481a7b5ec9de927b19669773cf4eaeab29
JulioErives <julioerives345@gmail.com>
2025-05-11 16:12:52 -0700

    fix: query fixed

--- registers/src/main/java/com/register/registers/repositories/PurchasesRepositoy.java
+++ registers/src/main/java/com/register/registers/repositories/PurchasesRepositoy.java
@@ -12,21 +12,14 @@ import com.register.registers.entities.Purchases;
 
 public interface PurchasesRepositoy extends JpaRepository<Purchases,Long>  {
     @Query("""
-        SELECT 
-            P.*,
-            PR.name,
-            PR.price,
-            PRT.name
-
-        FROM purchase P
-        JOIN products PR ON PR.user_id = :userId
-        JOIN product_type PRT ON PRT.user_id = :userId AND PRT.product_type_id = PR.product_type_id
-        WHERE (
-        (:searchType = 'description' AND P.description LIKE '%:name%')
-        OR
-        (:searchType = 'product' AND PR.name LIKE '%:name%')
-        OR
-        (:searchType = 'productType' AND PRT.name LIKE '%:name%')
+        SELECT p 
+        FROM Purchases p
+        JOIN p.product pr
+        JOIN pr.productType prt
+        WHERE p.user.id = :userId AND (
+            (:searchType = 'description' AND LOWER(p.description) LIKE LOWER(CONCAT('%', :name, '%')))
+            OR (:searchType = 'product' AND LOWER(pr.name) LIKE LOWER(CONCAT('%', :name, '%')))
+            OR (:searchType = 'productType' AND LOWER(prt.name) LIKE LOWER(CONCAT('%', :name, '%')))
         )
     """)
     public List<Purchases> findByFilters(@Param("name") String name, @Param("searchType") String searchType, Long userId, Pageable pageable );
