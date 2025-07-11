316208b38df99e2d8bcc7292d378335ebf1d8c2a
Julio Erives <julioerives345@gmail.com>
2025-07-11 12:33:43 -0600

    fix: add missing import statements for ProductTypeMapper

--- registers/src/main/java/com/register/registers/mappers/ProductTypeMapper.java
+++ registers/src/main/java/com/register/registers/mappers/ProductTypeMapper.java
@@ -1,6 +1,7 @@
 package com.register.registers.mappers;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import com.register.registers.dto.ProductTypeRequestDTO;
 import com.register.registers.entities.ProductType;
@@ -8,5 +9,7 @@ import com.register.registers.entities.Users;
 
 @Mapper(componentModel = "spring")
 public interface ProductTypeMapper {
+    @Mapping(target = "productTypeId", ignore = true)
+    @Mapping(target = "products", ignore = true)
     ProductType toEntity(ProductTypeRequestDTO productTypeRequestDTO, Users user);
 }
